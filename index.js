1e383105a9e3c66f2d19cca0a62fbff7fddc94a9
Shinya Yano <ssiinnyyaa55@gmail.com>
2021-02-22 09:45:28 +0900

    モジュールを追加

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict';
 
-module.exports = {
+function isMultipleOfSeventeen(num) {
+　　return num % 17 === 0;
 }
+
+module.exports = {
+    isMultipleOfSeventeen
+};
+/**
+ * 空の配列を設定
+ * 17で割り切れるかどうかを判定
+ * 　a =>　num % 17 === 0
+ * 
+ * 判定した数値を配列に入れる
+ */
